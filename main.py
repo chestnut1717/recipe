223692419efe1f9a5ed96e233084864c73a8e323
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2021-11-27 22:28:01 +0900

    update requirement, main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 
 import sys
 from streamlit import cli as stcli
-from streamlit import SystemExit 
 
 if __name__ == '__main__':
     # https://stackoverflow.com/questions/62760929/how-can-i-run-a-streamlit-app-from-within-a-python-script
     sys.argv = ["streamlit", "run", "view.py"]
     sys.exit(stcli.main())
-    
